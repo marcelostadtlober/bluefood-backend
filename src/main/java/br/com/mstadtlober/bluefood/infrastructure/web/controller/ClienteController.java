1a9f66559f65727e5086901304bd4a66652410c7
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2020-09-17 21:41:01 -0300

    Escolhendo os itens do cardápio do restaurante - Parte 3

--- src/main/java/br/com/mstadtlober/bluefood/infrastructure/web/controller/ClienteController.java
+++ src/main/java/br/com/mstadtlober/bluefood/infrastructure/web/controller/ClienteController.java
@@ -109,7 +109,9 @@ public class ClienteController {
 	}
 	
 	@GetMapping(path = "/restaurante")
-	public String viewRestaurante(@RequestParam("restauranteId") Integer restauranteId,
+	public String viewRestaurante(
+			@RequestParam("restauranteId") Integer restauranteId,
+			@RequestParam(value = "categoria", required = false) String categoria,
 			Model model) {
 		
 		Restaurante restaurante = restauranteRepository.findById(restauranteId).orElseThrow();
@@ -122,11 +124,18 @@ public class ClienteController {
 		List<ItemCardapio> itensCardapioDestaque;
 		List<ItemCardapio> itensCardapioNaoDestaque;
 		
-		itensCardapioDestaque = itemCardapioRepository.findByRestaurante_IdAndDestaqueOrderByNome(restauranteId, true);
-		model.addAttribute("itensCardapioDestaque", itensCardapioDestaque);
+		if (categoria == null) {
+			itensCardapioDestaque = itemCardapioRepository.findByRestaurante_IdAndDestaqueOrderByNome(restauranteId, true);
+			itensCardapioNaoDestaque = itemCardapioRepository.findByRestaurante_IdAndDestaqueOrderByNome(restauranteId, false);
+			
+		} else {
+			itensCardapioDestaque = itemCardapioRepository.findByRestaurante_IdAndDestaqueAndCategoriaOrderByNome(restauranteId, true, categoria);
+			itensCardapioNaoDestaque = itemCardapioRepository.findByRestaurante_IdAndDestaqueAndCategoriaOrderByNome(restauranteId, false, categoria);
+		}
 		
-		itensCardapioNaoDestaque = itemCardapioRepository.findByRestaurante_IdAndDestaqueOrderByNome(restauranteId, false);
+		model.addAttribute("itensCardapioDestaque", itensCardapioDestaque);
 		model.addAttribute("itensCardapioNaoDestaque", itensCardapioNaoDestaque);
+		model.addAttribute("categoriaSelecionada", categoria);
 		
 		return "cliente-restaurante";
 	}
