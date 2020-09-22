643585e9e52a08c4ad71480c4a1c7630c5a38100
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2020-09-22 00:21:14 -0300

    Implementando o carrinho de compras - Parte 3

--- src/main/java/br/com/mstadtlober/bluefood/infrastructure/web/controller/CarrinhoController.java
+++ src/main/java/br/com/mstadtlober/bluefood/infrastructure/web/controller/CarrinhoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import br.com.mstadtlober.bluefood.domain.pedido.Carrinho;
 import br.com.mstadtlober.bluefood.domain.pedido.RestauranteDiferenteException;
@@ -27,6 +28,11 @@ public class CarrinhoController {
 		return new Carrinho();
 	}
 	
+	@GetMapping(path = "/visualizar")
+	public String viewCarrinho() {
+		return "cliente-carrinho";
+	}
+	
 	@GetMapping(path = "/adicionar")
 	public String adicionarItem(
 			@RequestParam("itemId") Integer itemId,
@@ -42,6 +48,24 @@ public class CarrinhoController {
 			
 		} catch (RestauranteDiferenteException e) {
 			model.addAttribute("msg", "Não é possível misturar comidas de restaurantes diferentes");
+		}
+		
+		return "cliente-carrinho";
+	}
+	
+	@GetMapping(path = "/remover")
+	public String removerItem(
+			@RequestParam("itemId") Integer itemId,
+			@ModelAttribute("carrinho") Carrinho carrinho,
+			SessionStatus sessionStatus,
+			Model model) {
+		
+		ItemCardapio itemCardapio = itemCardapioRepository.findById(itemId).orElseThrow();
+		
+		carrinho.removerItem(itemCardapio);
+		
+		if (carrinho.vazio()) {
+			sessionStatus.setComplete();
 		}
 		
 		return "cliente-carrinho";
