488e57aa410b6c27ba7113b8dc68bb34d42994f4
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2020-09-23 20:16:13 -0300

    Criando o pedido com base nos itens do carrinho

---
+++ src/main/java/br/com/mstadtlober/bluefood/domain/pedido/PedidoRepository.java
@@ -0,0 +1,7 @@
+package br.com.mstadtlober.bluefood.domain.pedido;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
