389d6033b77baf9fd9ae34d3ba7f41edeee1c799
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2020-11-12 16:49:08 -0300

    Preparando o ambiente para a escrita dos testes

--- src/main/java/br/com/mstadtlober/bluefood/application/test/InsertDataForTesting.java
+++ src/main/java/br/com/mstadtlober/bluefood/application/test/InsertDataForTesting.java
@@ -9,6 +9,8 @@ import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import org.springframework.stereotype.Component;
 
 import br.com.mstadtlober.bluefood.domain.cliente.Cliente;
@@ -44,21 +46,26 @@ public class InsertDataForTesting {
 	
 	@EventListener
 	public void onApplicationEvent(ContextRefreshedEvent event) {
-		Cliente[] clientes = clientes();
-		@SuppressWarnings("unused")
-		Restaurante[] restaurantes = restaurantes();
-		itensCardapio(restaurantes);
+		Environment  environment = event.getApplicationContext().getEnvironment();
 		
-		Pedido p = new Pedido();
-		p.setData(LocalDateTime.now());
-		p.setCliente(clientes[0]);
-		p.setRestaurante(restaurantes[0]);
-		p.setStatus(Status.Producao);
-		p.setSubtotal(BigDecimal.valueOf(10));
-		p.setTaxaEntrega(BigDecimal.valueOf(2));
-		p.setTotal(BigDecimal.valueOf(12.0));
+		if (environment.acceptsProfiles(Profiles.of("dev"))) {
+			Cliente[] clientes = clientes();
+//			@SuppressWarnings("unused")
+			Restaurante[] restaurantes = restaurantes();
+			itensCardapio(restaurantes);
+			
+			Pedido p = new Pedido();
+			p.setData(LocalDateTime.now());
+			p.setCliente(clientes[0]);
+			p.setRestaurante(restaurantes[0]);
+			p.setStatus(Status.Producao);
+			p.setSubtotal(BigDecimal.valueOf(10));
+			p.setTaxaEntrega(BigDecimal.valueOf(2));
+			p.setTotal(BigDecimal.valueOf(12.0));
+			
+			pedidoRepository.save(p);			
+		}
 		
-		pedidoRepository.save(p);
 	}
 	
 	private Restaurante[] restaurantes() {
---
+++ src/test/java/br/com/mstadtlober/bluefood/CheckingTest.java
@@ -0,0 +1,11 @@
+package br.com.mstadtlober.bluefood;
+
+import org.junit.jupiter.api.Test;
+
+public class CheckingTest {
+
+	@Test
+	public void testSomething() throws Exception {
+		//... teste qualquer
+	}
+}
